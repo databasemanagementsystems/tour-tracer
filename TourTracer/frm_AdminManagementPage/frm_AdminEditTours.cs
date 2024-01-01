d4777366abec305d2c4faad121614c76aa2dbfad
Mel <103378309+MeltemNUzun@users.noreply.github.com>
2024-01-01 21:21:28 +0300

    meltem son değişiklik

--- TourTracer/frm_AdminManagementPage/frm_AdminEditTours.cs
+++ TourTracer/frm_AdminManagementPage/frm_AdminEditTours.cs
@@ -19,6 +19,8 @@ namespace TourTracer
 
         private void frm_EditSelectedTours_Load(object sender, EventArgs e)
         {
+            // TODO: Bu kod satırı 'tourTracerDataSet6.tbl_Cities' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.tbl_CitiesTableAdapter.Fill(this.tourTracerDataSet6.tbl_Cities);
 
         }
     }
